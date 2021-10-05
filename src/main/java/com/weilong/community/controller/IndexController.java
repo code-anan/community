d3074cad6e178e6566cd16fe24eec1766b36d30c
code-anan <719603766@qq.com>
2021-10-05 20:29:23 +0800

    first

---
+++ src/main/java/com/weilong/community/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.weilong.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String toHome(){
+        return "index";
+    }
+}
