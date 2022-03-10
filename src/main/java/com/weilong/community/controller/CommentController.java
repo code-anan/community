d666668e5077fc99a5b74d1af0f4cb5e0f31444a
code-anan <719603766Wqq.com>
2022-03-10 17:10:25 +0800

    3/10

--- src/main/java/com/weilong/community/controller/CommentController.java
+++ src/main/java/com/weilong/community/controller/CommentController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class CommentController {
--- src/main/resources/db/migration/V1__Add_question_and_user_table.sql
+++ src/main/resources/db/migration/V1__Add_question_and_user_table.sql
@@ -1,68 +1,59 @@
-create table question
-(
-    id            int auto_increment
-        primary key,
-    title         varchar(50),
-    description   text,
-    gmt_create    bigint,
-    gmt_modify    bigint,
-    author        int,
-    comment_count int default 0,
-    view_count    int default 0,
-    like_count    int default 0,
-    tag           varchar(256)
-);
-create table user
-(
-    id         int auto_increment
-        primary key,
-    account_id varchar(100),
-    name       varchar(50),
-    token      varchar(36),
-    gmt_create bigint,
-    gmt_modify bigint,
-    bio        varchar(256),
-    avator_url varchar(256)
-);
-create table comment
-(
-    id            int auto_increment,
-    parentid      int,
-    type          int,
-    commentauthor int not null,
-    gmtcreate     bigint,
-    gmtmodify     bigint,
-    likecount     int default 0,
-    content       varchar(1024),
-    constraint comment_pk
-        primary key (id)
-);
-comment on column comment.parentid is '回复的对象id';
+-- lwl.comment definition
 
-comment on column comment.type is '问题回复1评论回复2';
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `commentauthor` int(11) NOT NULL,
+  `gmtcreate` bigint(20) DEFAULT NULL,
+  `gmtmodify` bigint(20) DEFAULT NULL,
+  `likecount` int(11) DEFAULT '0',
+  `content` varchar(1024) DEFAULT NULL,
+  `commentcount` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20220315 DEFAULT CHARSET=utf8 COMMENT='回复的对象id';
 
-comment on column comment.commentauthor is '评论人';
+-- lwl.notification definition
 
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `notifier` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
+  `outerid` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `gmtcreate` bigint(20) DEFAULT NULL,
+  `status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-create table notification
-(
-    id        int not null,
-    notifier  int not null,
-    receiver  int not null,
-    outerid   int not null,
-    type      int not null,
-    gmtcreate bigint,
-    status    int default 0,
-    constraint notification_pk
-        primary key (id)
-);
+-- lwl.question definition
 
-comment on column notification.notifier is '发起人';
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `description` text,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modify` bigint(20) DEFAULT NULL,
+  `author` int(11) DEFAULT NULL,
+  `comment_count` int(11) DEFAULT '0',
+  `view_count` int(11) DEFAULT '0',
+  `like_count` int(11) DEFAULT '0',
+  `tag` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
-comment on column notification.receiver is '接收人';
+-- lwl.`user` definition
 
-comment on column notification.outerid is '外键id';
-
-comment on column notification.status is '0表示未读  1表示已读';
-
-
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `token` varchar(36) DEFAULT NULL,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modify` bigint(20) DEFAULT NULL,
+  `bio` varchar(256) DEFAULT NULL,
+  `avator_url` varchar(256) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
